b2bb3a9ae5375f86db2bd55eb4822e254194de01
Muhammed Aljailane <darhost56@gmail.com>
2021-08-23 19:48:30 +0300

    Add files via upload

---
+++ adsense.php
@@ -0,0 +1,18 @@
+<?php 
+// تطوير محمد الجيلاني 
+// تاريخ 2021/08/20
+// v2.2
+
+include('config.php'); 
+?>
+<center>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="<?=$adsense_pubid?>"
+     data-ad-slot="<?=$adsense_adid?>"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
